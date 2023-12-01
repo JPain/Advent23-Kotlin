984e3a954b00850c50b60e4f95004ff1c9abb3ce
James Pain <jamesepain@gmail.com>
2023-12-01 19:52:00 +0000

    Day01 Solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        for (item in input) {
+            var digit1 = 0
+            var digit2 = 0
+
+            for (letter in item) {
+                if (letter.isDigit()) {
+                    digit1 = letter.digitToInt()
+                }
+            }
+
+            for (letter in item.reversed()) {
+                if (letter.isDigit()) {
+                    digit2 = letter.digitToInt()
+                }
+            }
+
+            val wholeNumber = (digit2.toString() + digit1.toString()).toInt()
+
+            result += wholeNumber
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
@@ -8,8 +29,8 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
